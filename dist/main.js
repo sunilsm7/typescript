262a50357256095c37badda669c3631bff561dd0
Sunil <sunil9766@gmail.com>
2017-08-30 17:46:03 +0530

    declaring variables let vs var

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,15 @@
-var SweetSweetClass = (function () {
-    function SweetSweetClass() {
-        console.log("it's sweat");
+var hello = "Hii";
+var hi = 'Hello';
+hello = 123;
+var abc = "another";
+console.log("Hello world");
+function f(input) {
+    var a = 100;
+    if (input) {
+        var b = a + 10012;
+        return b;
     }
-    return SweetSweetClass;
-}());
-var basil = new SweetSweetClass();
+    return a;
+}
+console.log(f(true));
+console.log(f(false));
--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
-class SweetSweetClass {
-    constructor() { 
-        console.log("it's sweat")
+
+let hello:any = "Hii"
+var hi = 'Hello'
+
+hello = 123
+let abc = "another"
+console.log("Hello world")
+
+function f(input:boolean){
+    let a= 100
+    if(input){
+        let b = a + 10012
+        return b;
     }
+    return a;
 }
-let basil = new SweetSweetClass()
+
+console.log(f(true))
+console.log(f(false))
