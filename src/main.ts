62beb2361c8a54200be42d4cfd4032921036af64
Sunil <sunil9766@gmail.com>
2017-08-31 16:40:05 +0530

    setup webpack

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { MustHaveCoffee } from './coffee/getcoffee'
+
+class ReallyCoffee extends MustHaveCoffee{
+    coffeeType = "Bulletproof"
+    constructor(){
+        super()
+    }
+    f(input:boolean){
+        let a= 100
+        if(input){
+            let b = a + 10012
+            return b;
+        }
+        return a;
+    }
+    setCoffeeType(name:string){
+        //super.setCoffeeType(name)
+        console.log("hello Coffee Type is " + this.coffeeType)
+    }
+    
+}
+
+let newCoffee = new ReallyCoffee()
+console.log(newCoffee.f(true))
+newCoffee.setCoffeeType("thick chocolate")
+let oldCoffee = new MustHaveCoffee()
